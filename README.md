5169c2b71147f1bbe22900e47c3f611f8e24eab7
FearIndigo <josephm@fearindigo.com.au>
2022-07-26 02:18:39 +0800

    -results 01

--- README.md
+++ README.md
@@ -8,3 +8,35 @@ pip3 install -r requirements.txt
 
 py traffic.py gtsrb
 ```
+
+---
+## Experimentation Process
+The process used to find the best outcome for the task was to run the training and inference with
+different parameters as per the table bellow. The x-axis represents the number of dense (hidden)
+layers in the network, and how many units they have. The y-axis is the number of convolutional layers,
+how many filters they have and whether max-pooling was used after each layer. The values in the table
+are (training accuracy / test accuracy) averaged over 3 runs of 10 epochs.
+
+| Filters-Pooling-Layers \ Units-Layers | 0-0 | 128-1 | 128-2 | 128-3 | 256-1 | 256-2 | 256-3 | 512-1 | 512-2 | 512-3 |
+|--------------------------------------:|----:|------:|------:|------:|------:|------:|------:|------:|------:|------:|
+|                             0-false-0 |     |       |       |       |       |       |       |       |       |       |
+|                              0-true-0 |     |       |       |       |       |       |       |       |       |       |
+|                             8-false-1 |     |       |       |       |       |       |       |       |       |       |
+|                              8-true-1 |     |       |       |       |       |       |       |       |       |       |
+|                            16-false-1 |     |       |       |       |       |       |       |       |       |       |
+|                             16-true-1 |     |       |       |       |       |       |       |       |       |       |
+|                            32-false-1 |     |       |       |       |       |       |       |       |       |       |
+|                             32-true-1 |     |       |       |       |       |       |       |       |       |       |
+|                             8-false-2 |     |       |       |       |       |       |       |       |       |       |
+|                              8-true-2 |     |       |       |       |       |       |       |       |       |       |
+|                            16-false-2 |     |       |       |       |       |       |       |       |       |       |
+|                             16-true-2 |     |       |       |       |       |       |       |       |       |       |
+|                            32-false-2 |     |       |       |       |       |       |       |       |       |       |
+|                             32-true-2 |     |       |       |       |       |       |       |       |       |       |
+|                             8-false-3 |     |       |       |       |       |       |       |       |       |       |
+|                              8-true-3 |     |       |       |       |       |       |       |       |       |       |
+|                            16-false-3 |     |       |       |       |       |       |       |       |       |       |
+|                             16-true-3 |     |       |       |       |       |       |       |       |       |       |
+|                            32-false-3 |     |       |       |       |       |       |       |       |       |       |
+|                             32-true-3 |     |       |       |       |       |       |       |       |       |       |
+
--- traffic.py
+++ traffic.py
@@ -12,6 +12,51 @@ IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
 
+X_PARAMS = [
+    (0, 0),
+    (128, 1),
+    (128, 2),
+    (128, 3),
+    (256, 1),
+    (256, 2),
+    (256, 3),
+    (512, 1),
+    (512, 2),
+    (512, 3)
+]
+
+Y_PARAMS = [
+    (0, False, 0),
+    (0, True, 0),
+    (8, False, 1),
+    (8, True, 1),
+    (16, False, 1),
+    (16, True, 1),
+    (32, False, 1),
+    (32, True, 1),
+    (8, False, 2),
+    (8, True, 2),
+    (16, False, 2),
+    (16, True, 2),
+    (32, False, 2),
+    (32, True, 2),
+    (8, False, 3),
+    (8, True, 3),
+    (16, False, 3),
+    (16, True, 3),
+    (32, False, 3),
+    (32, True, 3)
+]
+
+DENSE_LAYERS = 0
+DENSE_UNITS = 0
+
+CONV_LAYERS = 0
+CONV_FILTERS = 0
+
+MAX_POOLING = False
+
+NUM_SAMPLES = 3
 
 def main():
 
@@ -24,18 +69,48 @@ def main():
 
     # Split data into training and testing sets
     labels = tf.keras.utils.to_categorical(labels)
-    x_train, x_test, y_train, y_test = train_test_split(
-        np.array(images), np.array(labels), test_size=TEST_SIZE
-    )
 
-    # Get a compiled neural network
-    model = get_model()
 
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
+    results = dict()
+    global CONV_FILTERS
+    global MAX_POOLING
+    global CONV_LAYERS
+    global DENSE_UNITS
+    global DENSE_LAYERS
 
-    # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
+    for y in Y_PARAMS:
+        CONV_FILTERS = y[0]
+        MAX_POOLING = y[1]
+        CONV_LAYERS = y[2]
+        for x in X_PARAMS:
+            DENSE_UNITS = x[0]
+            DENSE_LAYERS = x[1]
+
+            for i in range(NUM_SAMPLES):
+
+                x_train, x_test, y_train, y_test = train_test_split(
+                    np.array(images), np.array(labels), test_size=TEST_SIZE
+                )
+
+                # Get a compiled neural network
+                model = get_model()
+
+                # Fit model on training data
+                training = model.fit(x_train, y_train, epochs=EPOCHS)
+
+                # Evaluate neural network performance
+                testing = model.evaluate(x_test,  y_test, verbose=2)
+
+                if (x, y) in results:
+                    temp = results.get((x, y))
+                    results[(x, y)] = (temp[0] + training.history['accuracy'][-1], temp[1] + testing[-1])
+                else:
+                    results[(x, y)] = (training.history['accuracy'][-1], testing[-1])
+            temp = results.get((x, y))
+            results[(x, y)] = (temp[0] / NUM_SAMPLES, temp[1] / NUM_SAMPLES)
+
+    for res in results:
+        print(f"{res}: {results[res]}")
 
     # Save model to file
     if len(sys.argv) == 3:
@@ -87,27 +162,20 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    # Create model
+    # Create model, with input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
     model = tf.keras.Sequential([
-        # Input layer, with input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         tf.keras.layers.InputLayer(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
-
-        # Convolutional layer, Learn 32 filters using a 3x3 kernel
-        tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
-
-        # Max pooling layer, using a 2x2 pool size
-        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
-
-        # Flatten units
-        tf.keras.layers.Flatten(),
-
-        # Add hidden layer with 128 units, and dropout
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.5),
-
-        # Add output layer, which represents probability distribution of each image category
-        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
+
+    model = add_conv(model)
+
+    model.add(tf.keras.layers.Flatten())
+
+    model = add_dense(model)
+
+    # Add output layer, which represents probability distribution of each image category
+    model.add(tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax"))
+
     # Compile model
     model.compile(
         optimizer="adam",
@@ -115,6 +183,27 @@ def get_model():
         metrics=["accuracy"]
     )
     # Return compiled model
+    return model
+
+
+def add_conv(model):
+    print(f"Adding {CONV_LAYERS} convolution layers with {CONV_FILTERS} filters and {'' if MAX_POOLING else 'no '}max-pooling...")
+    if CONV_LAYERS == 0 and MAX_POOLING:
+        model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+    else:
+        for i in range(CONV_LAYERS):
+            model.add(tf.keras.layers.Conv2D(CONV_FILTERS, (3, 3), activation="relu"))
+            if MAX_POOLING:
+                model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+    return model
+
+
+def add_dense(model):
+    print(
+        f"Adding {DENSE_LAYERS} hidden layers with {DENSE_UNITS} units in each...")
+    for i in range(DENSE_LAYERS):
+        model.add(tf.keras.layers.Dense(DENSE_UNITS, activation="relu"))
+        # model.Add(tf.keras.layers.Dropout(0.5))
     return model
 
 
